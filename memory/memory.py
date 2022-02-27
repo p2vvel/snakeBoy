a43105e5a7e4f2bca4a2e366c686b801807bcdab
p2vvel <p2vvel@gmail.com>
2022-02-27 13:56:11 +0100

    Rewrite memory base
    Memory operators use assert instead of try, catch now. Rewrite tests to fit to new implementation

--- memory/memory.py
+++ memory/memory.py
@@ -2,26 +2,26 @@ class Memory:
     def __init__(self):
         self.memory = [0] * 0x10000
 
-    def __getitem__(self, item):
+    def __getitem__(self, address: int) -> int:
         """Get memory byte value at 'item' address'"""
-        try:
-            # byte value should be an integer in range <0, 255>
-            assert isinstance(self.memory[item], int), f"Incorrect byte type: {type(self.memory[item])} [{self.memory[item]}]"
-            assert 0 <= self.memory[item] <= 0xff, f"Incorrect byte value: {self.memory[item]}"
-            assert item >= 0, f"Negative byte index value: {item}"    # only integer values will be assigned to bytes
+        # check if address is integer
+        assert isinstance(address, int), f"Wrong memory address type: {type(address)}"
+        # check if address in range <0, 0xffff>
+        assert 0 <= address <= 0xffff, f"Memory address out of bounds: {hex(address)}"
+        # check if data at specified memory address is integer
+        assert isinstance(self.memory[address], int), f"Wrong byte type: {type(self.memory[address])} [{self.memory[address]}]"
+        # check if data is stores proper byte value (in range <0, 255>)
+        assert 0 <= self.memory[address] <= 0xff, f"Incorrect byte value: {self.memory[address]}"
 
-            return self.memory[item]
-        except (IndexError, TypeError):
-            print(f"Trying to access memory at wrong address: {item}")
-            raise       # passing exception on
+        return self.memory[address]
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, address: int, value: int) -> None:
         """Set 'value' byte at memory address 'key'"""
-        try:
-            assert isinstance(value, int), f"Incorrect byte type: {type(value)} [{value}]"    # only integer values will be assigned to bytes
-            assert key >= 0, f"Negative byte index value: {key}"    # only integer values will be assigned to bytes
+        # check if address is integer
+        assert isinstance(address, int), f"Incorrect address type: {type(address)} [{address}]"
+        # check if address is in proper range
+        assert 0 <= address <= 0xffff, f"Wrong address: {hex(address)}"    # only integer values will be assigned to bytes
+        # check if new value is integer
+        assert isinstance(value, int), f"Incorrect data type: {type(value)} [{value}]"
 
-            self.memory[key] = value & 0xff
-        except (IndexError, TypeError):
-            print(f"Trying to access memory at wrong address: {key}")
-            raise       # passing exception on
+        self.memory[address] = value & 0xff
--- memory/tests/test_operators.py
+++ memory/tests/test_operators.py
@@ -5,6 +5,30 @@ class TestOperators(TestCase):
     def setUp(self) -> None:
         self.memory = Memory()
 
+    def test_memory_read_operator(self):
+        """__getitem__ operator should return memory value at given address"""
+        memory = self.memory
+        self.assertEqual(memory[0], 0)   # 0 is initial value
+
+        memory.memory[0] = 0xff     # writing data at given address
+        self.assertEqual(memory[0], 0xff)
+
+        # trying to access unavailable memory should raise an exception
+        self.assertRaises(AssertionError, lambda: memory[0x10000])
+        # allowing for negative addresses would make code harder to debug
+        self.assertRaises(AssertionError, lambda: memory[-1])
+        # non-integer addresses aren't allowed
+        self.assertRaises(AssertionError, lambda: memory["xd"])
+        # non-integer data stored in memory would indicate a BIG problem in CPU implementation
+        memory.memory[0] = "test"
+        self.assertRaises(AssertionError, lambda: memory[0])
+        # storing value out of range <0, 255> could be a problem
+        memory.memory[0] = 0x100
+        self.assertRaises(AssertionError, lambda: memory[0])
+        memory.memory[0] = -1
+        self.assertRaises(AssertionError, lambda: memory[0])
+
+
     def test_memory_write_operator(self):
         """__setitem__ operator should write one byte of data at specified memory address"""
         memory = self.memory
@@ -19,8 +43,6 @@ class TestOperators(TestCase):
         # assigning non-int value should cause exception raise
         with self.assertRaises(AssertionError):
             memory[0] = "wrong type"
-
-        # assigning non-int value should cause exception raise
         with self.assertRaises(AssertionError):
             memory[0] = 2.15
 
@@ -28,17 +50,6 @@ class TestOperators(TestCase):
         with self.assertRaises(AssertionError):
             memory[-1] = 12
 
-    def test_memory_read_operator(self):
-        """__getitem__ operator should return memory value at given address"""
-        memory = self.memory
-        self.assertEqual(memory[0], 0)   # 0 is initial value
-
-        memory.memory[0] = 0xff     # writing data at given address
-        self.assertEqual(memory[0], 0xff)   # 0 is initial value
-
-        # trying to access unavailable memory should raise an exception
-        self.assertRaises(IndexError, lambda: memory[0x10000])
-        self.assertRaises(TypeError, lambda: memory["xd"])
-        memory.memory[0] = "test"
-        self.assertRaises(AssertionError, lambda: memory[0])    # byte should never be a non-string
-        self.assertRaises(AssertionError, lambda: memory[-1])   # allowing for negative addresses would make code harder to debug
+        # wrong address type
+        with self.assertRaises(AssertionError):
+            memory["xd"] = 12
